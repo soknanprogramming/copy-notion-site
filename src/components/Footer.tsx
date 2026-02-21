5e7319a05a0479dd0eac92fe971037e2e76808ca
soknanprogramming <poch.soknan@gmail.com>
2026-02-22 05:25:21 +0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import NotionBar from './Footer/NotionBar'
 import CardFooter from './Footer/CardFooter'
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
-    <footer className='flex border-t border-gray-300'>
-        <NotionBar className='w-max-72'/>
-        <CardFooter title='Company'>
-            <Link to="/"><p>About us</p></Link>
-            <Link to="/"><p>Careers</p></Link>
-            <Link to="/"><p>Security</p></Link>
-            <Link to="/"><p>Status</p></Link>
-            <Link to="/"><p>Terms & privacy</p></Link>
-            <Link to="/"><p>Your privacy rights</p></Link>
-        </CardFooter>
+    <footer className='flex px-35 py-10 justify-between border-t border-gray-300'>
+      <NotionBar className='w-max-72' />
+      <CardFooter title='Company'>
+        <Link to="/"><p>About us</p></Link>
+        <Link to="/"><p>Careers</p></Link>
+        <Link to="/"><p>Security</p></Link>
+        <Link to="/"><p>Status</p></Link>
+        <Link to="/"><p>Terms & privacy</p></Link>
+        <Link to="/"><p>Your privacy rights</p></Link>
+      </CardFooter>
+      <CardFooter title='Download'>
+        <Link to="/"><p>IOS & Android</p></Link>
+        <Link to="/"><p>Mac & Windows</p></Link>
+        <Link to="/"><p>Mail</p></Link>
+        <Link to="/"><p>Calender</p></Link>
+        <Link to="/"><p>Web Clipper</p></Link>
+      </CardFooter>
+      <CardFooter title='Resources'>
+        <Link to="/"><p>Help center</p></Link>
+        <Link to="/"><p>Pricing</p></Link>
+        <Link to="/"><p>Blog</p></Link>
+        <Link to="/"><p>Community</p></Link>
+        <Link to="/"><p>Integration</p></Link>
+        <Link to="/"><p>Templates</p></Link>
+        <Link to="/"><p>Partner programs</p></Link>
+      </CardFooter>
+      <CardFooter title='Notion for' foot={<Link to="/" className='flex items-center'><p className='whitespace-nowrap mr-1.5 font-bold'>Explore more </p><FaLongArrowAltRight/></Link>}>
+        <Link to="/"><p>Enterprise</p></Link>
+        <Link to="/"><p>Small business</p></Link>
+        <Link to="/"><p>Personal</p></Link>
+      </CardFooter>
     </footer>
   )
 }
--- src/components/Footer/CardFooter.tsx
+++ src/components/Footer/CardFooter.tsx
@@ -4,15 +4,19 @@ interface Prop {
     className?: string;
     title: string;
     children: React.ReactNode;
+    foot?: React.ReactNode;
 }
 
 
-const CardFooter: React.FC<Prop> = ({ className = "", title, children }) => {
+const CardFooter: React.FC<Prop> = ({ className = "", title, children, foot }) => {
   return (
-    <div className={`p-2 ${className}`}>
+    <div className={`p-2 h-min-48 py-4 relative ${className}`}>
         <h1 className='font-medium'>{title}</h1>
         <div className='[&>*>*]:text-gray-500 [&>*>*]:text-sm [&>*>*]:py-1'>
             {children}
+        </div>
+        <div className='absolute bottom-4'>
+          {foot}
         </div>
     </div>
   )
